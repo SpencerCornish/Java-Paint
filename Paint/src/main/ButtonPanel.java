993dea22654d88bd85b5720221fc78d74931ed58
Spencer Cornish <spencerjcornish@gmail.com>
2016-02-03 16:53:41 -0700

    Adding Buttons

--- Paint/src/main/ButtonPanel.java
+++ Paint/src/main/ButtonPanel.java
@@ -11,22 +11,34 @@ import java.awt.event.*;
  */
 public class ButtonPanel extends JPanel implements ActionListener {
 
-    public static ButtonPanel inst;
-    private JButton myButton;
+    public static ButtonPanel btnPnl; // Instance of Button Panel
+    private JButton buttonClear; // Clear the screen
+    private JButton buttonRectangleF; // Draw a Filled Rectangle
+    private JButton buttonRectangleE; // Draw a Filled Rectangle
+    private JButton buttonOvalF; // Draw a Filled Oval
+    private JButton buttonOvalE; // Draw an Empty Oval 
+    private JButton buttonLineD; // Draw a line
 
     ButtonPanel()
     {
-        setBackground(Color.YELLOW);
-        myButton = new JButton("Do Not Click Me");
-        myButton.addActionListener(this);
-        add(myButton);
+        setBackground(Color.GRAY);
+        buttonClear = new JButton("Clear");
+        buttonRectangleF = new JButton("Filled Rectangle");
+        buttonRectangleE = new JButton("Rectangle");
+        buttonOvalF = new JButton("Filled Rectangle");
+        buttonOvalE = new JButton("Filled Rectangle");
+        
+        
+        add(buttonClear);
+        buttonClear.addActionListener(this);
+        add(buttonClear);
 
     }
      public static ButtonPanel getInstance()
     {
-        if(inst == null)
-           inst =  new ButtonPanel();
-        return inst;
+        if(btnPnl == null)
+           btnPnl =  new ButtonPanel();
+        return btnPnl;
     }
 
      public void actionPerformed(ActionEvent ae)
