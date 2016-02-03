fd2b4939ef8f7423928df1bbc9baa64fbbdb490b
Nate Tranel <njtranel@gmail.com>
2016-02-03 16:07:41 -0700

    added class

---
+++ Paint/src/main/Frame.java
@@ -0,0 +1,15 @@
+package main;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Frame extends JFrame {
+	
+	public static Frame inst;
+	
+	Frame() {
+		super("Paint Program");
+		
+	}
+	
+}
