0766afecf0fb944e829b586c253694b89c20365e
Spencer Cornish <spencerjcornish@gmail.com>
2016-02-03 11:20:23 -0700

    Skeleton program ready for actual code!

--- Paint/src/main/Main.java
+++ Paint/src/main/Main.java
@@ -3,8 +3,6 @@ package main;
 public class Main {
 
 	public static void main(String[] args){
-		System.out.println("Hello!");
-		System.out.println("Still Testing Commits!");
-		//test from desktop to laptop
+
 	}
 }
