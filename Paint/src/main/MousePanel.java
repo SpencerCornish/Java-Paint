10cc49ad7799209c5df46eae45bfb694d4b12520
Spencer Cornish <spencerjcornish@gmail.com>
2016-02-03 22:20:02 -0700

    - All shapes now have live drag and drop ability. - Fixed comments - Clear now resets basic coordinates - Changed positioning coordinates for big shapes for look-and-feel reasons

--- Paint/src/main/MousePanel.java
+++ Paint/src/main/MousePanel.java
@@ -10,19 +10,18 @@ import java.awt.event.*;
 import java.awt.image.BufferedImage;
 import java.util.Random;
 
-public class MousePanel extends JPanel implements MouseListener /*, MouseMotionListener */{	//maybe need to use motion listener to draw stuff
+public class MousePanel extends JPanel implements MouseListener, MouseMotionListener {	//maybe need to use motion listener to draw stuff
     //BufferedImage grid;
     //Graphics2D gc;
     public static MousePanel inst;
     private int button = -1;	//determines which button is pressed based on a number
     Point sPoint = new Point(-1, -1); 
     Point ePoint = new Point(-1, -1);
-    
     MousePanel()
     {
         setBackground(Color.GREEN);
         addMouseListener(this);
-        //addMouseMotionListener(this);
+        addMouseMotionListener(this);
     }
     public static MousePanel getInstance()
     {
@@ -46,11 +45,17 @@ public class MousePanel extends JPanel implements MouseListener /*, MouseMotionL
         /
         g2.drawImage(grid, null, 0, 0); */
         switch(button){
-        case 0: MousePanel.getInstance().removeAll(); break;
-        case 1: g2.fillRect(100, 200, 200, 100); break;	//draw filled rectangle
-        case 2: g2.drawRect(100, 200, 200, 100); break; //draw empty rectangle
-        case 3: g2.fillOval(100, 200, 200, 200); break; //draw filled oval
-        case 4: g2.drawOval(100, 200, 200, 200); break;	//draw empty oval
+        case 0: 
+        	MousePanel.getInstance().removeAll(); 
+        	ePoint.x = -1; //Cleans line tracking variables for next line made
+        	ePoint.y = -1;
+            sPoint.x = -1;  
+            sPoint.y = -1;
+        break;  // The following shapes have weird offsets,as to make the dragging of a shape feel less insane!
+        case 1: g2.fillRect(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); repaint(); break;	//draw filled rectangle
+        case 2: g2.drawRect(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); repaint(); break; //draw empty rectangle
+        case 3: g2.fillOval(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); repaint(); break; //draw filled oval
+        case 4: g2.drawOval(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); repaint(); break;	//draw empty oval
         case 5: g2.drawLine(sPoint.x, sPoint.y, ePoint.x, ePoint.y); repaint(); break; 
         }
         
@@ -59,9 +64,7 @@ public class MousePanel extends JPanel implements MouseListener /*, MouseMotionL
     public void setButton(int button) {
     	this.button = button;
     }
-    public void mouseExited(MouseEvent e){System.out.println("mouse exited");}
-    public void mouseEntered(MouseEvent e){System.out.println("mouse entered");}
-    public void mouseClicked(MouseEvent e){System.out.println("mouse clicked");}
+
     public void mousePressed(MouseEvent e){
     	System.out.println("mouse pressed");
     	e.consume();  
@@ -78,6 +81,15 @@ public class MousePanel extends JPanel implements MouseListener /*, MouseMotionL
         ePoint.y = e.getY();
         repaint();
     	}
-	//public void mouseDragged(MouseEvent e) {//System.out.println("mouse dragged");} //There are here because of implementations
-	//public void mouseMoved(MouseEvent e) {//System.out.println("mouse moved");}
+	public void mouseDragged(MouseEvent e) { //makes the line a live-drag
+    	e.consume();  
+        ePoint.x = e.getX();  
+        ePoint.y = e.getY();
+        repaint();
+		
+		}
+	public void mouseMoved(MouseEvent e) { } //This will be useful soon, adding mouse coords to a tooltip in the bottom right corner    
+	public void mouseExited(MouseEvent e){System.out.println("mouse exited");}
+    public void mouseEntered(MouseEvent e){System.out.println("mouse entered");}
+    public void mouseClicked(MouseEvent e){System.out.println("mouse clicked");}
 }
