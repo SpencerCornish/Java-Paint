634c4a39c3f5bd60245466c933904e753958f7ad
Nate Tranel <njtranel@gmail.com>
2016-02-08 10:04:56 -0700

    fixed some things, beginning to merge some new code

--- Paint/src/main/MousePanel.java
+++ Paint/src/main/MousePanel.java
@@ -2,6 +2,7 @@ package main;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.Point;
 import javax.swing.JPanel;
 import java.awt.event.*;
@@ -15,10 +16,17 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
     private int button = -1;						//determines which button is pressed based on a number
     Point sPoint = new Point(-1, -1);  				//Points used to align shapes and mouse drag
     Point ePoint = new Point(-1, -1);
-    private ArrayList<Shape> shapes = new ArrayList<Shape>();
+    private Image image;
     
-    public MousePanel()
-    {
+    public MousePanel() {
+        setDoubleBuffered(false);
+        addMouseListener(new MouseAdapter() {
+          public void mousePressed(MouseEvent e) {
+            // save coord when mouse is pressed
+            sPoint.x = e.getX();
+            sPoint.y = e.getY();
+          }
+        });
         setBackground(Color.WHITE); 			// Background Color for canvas
         addMouseListener(this); 				//Used to do live track, etc.
         addMouseMotionListener(this); 
@@ -33,7 +41,7 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 
     public void paintComponent(Graphics g)
     {
-        //super.paintComponent(g); 				//Inherits Graphics
+        super.paintComponent(g); 				//Inherits Graphics
         Graphics2D g2 = (Graphics2D)g;
         switch(button){   						// Switch on which button was pressed.  There may be a better way
         case 0: 
@@ -41,16 +49,13 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
         	ePoint.y = -1;
             sPoint.x = -1;  
             sPoint.y = -1;
-            shapes.clear();
             break;  // The following shapes have weird offsets,as to make the dragging of a shape feel less insane!
-        //case 1: Shape rF = new Shape(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); shapes.add(rF); rF.draw(g2, 1); break;		// Draw filled rectangle
-        case 1: Rectangle2D r2 = new Rectangle2D.Float(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y);  g2.fill(r2); break;
-        case 2: Shape rE = new Shape(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); rE.draw(g2, 2); shapes.add(rE); break; 		// Draw empty rectangle
-        case 3: Shape oF = new Shape(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); oF.draw(g2, 3); shapes.add(oF); break; 		// Draw filled oval
-        case 4: Shape oE = new Shape(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); oE.draw(g2, 4); shapes.add(oE); break;		// Draw empty oval
-        case 5: Shape ln = new Shape(sPoint.x, sPoint.y, ePoint.x, ePoint.y); ln.draw(g2, 5); shapes.add(ln); break; 						// Draw Line
+        case 1: Shape rF = new Shape(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); rF.draw(g2, 1); break;		// Draw filled rectangle
+        case 2: Shape rE = new Shape(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); rE.draw(g2, 2); break; 		// Draw empty rectangle
+        case 3: Shape oF = new Shape(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); oF.draw(g2, 3); break; 		// Draw filled oval
+        case 4: Shape oE = new Shape(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y); oE.draw(g2, 4); break;		// Draw empty oval
+        case 5: Shape ln = new Shape(sPoint.x, sPoint.y, ePoint.x, ePoint.y); ln.draw(g2, 5); break; 						// Draw Line
         }
-        repaint();
     }
     
     public void setButton(int button) {  			// Sets our button tracking variable
@@ -80,11 +85,11 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
         //repaint();
     	}
 	public void mouseDragged(MouseEvent e) { 		//makes the shape a live-drag
-    	//e.consume();  
-        //ePoint.x = e.getX();  
-        //ePoint.y = e.getY();
-        //System.out.println(e.getY() + " x " + e.getX());
-        //repaint();
+    	e.consume();  
+        ePoint.x = e.getX();  
+        ePoint.y = e.getY();
+        System.out.println(e.getY() + " x " + e.getX());
+        repaint();
 		}
 	public void mouseMoved(MouseEvent e) { } //This will be useful soon, adding mouse coords to a tooltip in the bottom right corner    
 	public void mouseExited(MouseEvent e){//System.out.println("mouse exited");
