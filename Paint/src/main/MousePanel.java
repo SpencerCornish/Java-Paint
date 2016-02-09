a0b5b09dd05c47df45c01775ad21ea2f954710ec
Nate Tranel <njtranel@gmail.com>
2016-02-09 11:54:18 -0700

    cleaned up stuff for dropbox

--- Paint/src/main/MousePanel.java
+++ Paint/src/main/MousePanel.java
@@ -30,98 +30,76 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 		buffer.setColor(Color.WHITE);
 		buffer.fillRect(0, 0, bufferImg.getWidth(),bufferImg.getHeight());
 
-		addMouseListener(this); 				//Used to get coordinates of shape
-		addMouseMotionListener(this); 			//Used for mouseDragged()
-		repaint(); 								//Repaints panel to initialize
-	}
-	public static MousePanel getInstance() 	// Returns Instance of MousePanel
-	{
+		addMouseListener(this); 					//Used to get coordinates of shape
+		addMouseMotionListener(this); 				//Used for mouseDragged()
+		repaint(); } 								//Repaints panel to initialize
+	
+	public static MousePanel getInstance() { 		// Returns Instance of MousePanel
 		if(mouseP == null) 						
 			mouseP =  new MousePanel(); 		
-		return mouseP; 							
-	}
+		return mouseP; }
 
-	public void paintComponent(Graphics g)	//no need to call superclass.
-	{
-		if (bufferImg == null) {			//fills the buffer if it hasn't been filled already
+	public void paintComponent(Graphics g) {
+		if (bufferImg == null) {					//fills the buffer if it hasn't been filled already
 			bufferImg = (BufferedImage) createImage(getSize().width, getSize().height);
 			g = (Graphics2D) bufferImg.getGraphics();
-			clearAll();
-		}
-
+			clearAll(); }
+		
 		RenderingHints rh = new RenderingHints(RenderingHints.KEY_ANTIALIASING,RenderingHints.VALUE_ANTIALIAS_ON);	//Turns on Antialiasing
 		RenderingHints rh2 = new RenderingHints(RenderingHints.KEY_RENDERING,RenderingHints.VALUE_RENDER_QUALITY);  //Uses quality rendering
 		((Graphics2D) g).setRenderingHints(rh); 		//Sets Hints
 		((Graphics2D) g).setRenderingHints(rh2);
-		if(paintStatusFlag == true)						//Used for the live preview
-		{
-			g.drawImage(bufferImgLive, 0, 0, null);
-		}
-		else
-		{
-			g.drawImage(bufferImg, 0, 0, null);			
-		}
-
-	}
-
-	public void setButton(int button) {  			// Sets our button tracking variable
-		this.button = button;
-	}
-	public void clearAll()		//clears mousepanel by painting over the background image
-	{
+		
+		if(paintStatusFlag == true) {					//used for live preview
+			g.drawImage(bufferImgLive, 0, 0, null); }
+		else {
+			g.drawImage(bufferImg, 0, 0, null);	} }
+	
+	public void setButton(int button) {  				// Sets our button tracking variable
+		this.button = button; }
+	
+	public void clearAll() { 							//clears mousepanel by painting over the background image
 		Graphics buffer = bufferImg.createGraphics();
 		buffer.setColor(Color.WHITE);
 		buffer.fillRect(0, 0, bufferImg.getHeight(), bufferImg.getWidth());
-		repaint();
-	}
-	public void fixDirections()	//allows user to draw from any direction
-	{
+		repaint(); }
+	public void fixDirections()	{						//allows user to draw from any direction
+		
 		if(button != 5){
 			if(startP.x > ePoint.x){
 				int sTemp = startP.x;
 				startP.x = ePoint.x;
-				ePoint.x = sTemp;
-			}
-			if(startP.y > ePoint.y){
+				ePoint.x = sTemp; }
+			if(startP.y > ePoint.y) {
 				int sTemp = startP.y;
 				startP.y = ePoint.y;
-				ePoint.y = sTemp;
-			}
-		}
-
-	}
-	public void fixLiveDirections()	
-	{
+				ePoint.y = sTemp; } } }
+	
+	public void fixLiveDirections()	{					//makes shape not mess up when dragging from other directions
 		if(button != 5){
 			if(sPoint.x > ePoint.x){
 				int sTemp = sPoint.x;
 				sPoint.x = ePoint.x;
-				ePoint.x = sTemp;
-			}
+				ePoint.x = sTemp; }
 			if(sPoint.y > ePoint.y){
 				int sTemp = sPoint.y;
 				sPoint.y = ePoint.y;
-				ePoint.y = sTemp;
-			}
-		}
-
-	}
+				ePoint.y = sTemp; } } }
+	
 	public static BufferedImage deepCopy(BufferedImage bi) {
 		ColorModel cm = bi.getColorModel();
 		boolean isAlphaPremultiplied = cm.isAlphaPremultiplied();
 		WritableRaster raster = bi.copyData(null);
-		return new BufferedImage(cm, raster, isAlphaPremultiplied, null);
-	}
-
+		return new BufferedImage(cm, raster, isAlphaPremultiplied, null);}
+	
 	public void mousePressed(MouseEvent e){			// Initial coords for shape
-		System.out.println("mouse pressed");
 		e.consume();
 		sPoint.x = e.getX();  						// Sets start points
 		sPoint.y = e.getY();
 		startP.x = sPoint.x;
 		startP.y = sPoint.y;
-		paintStatusFlag = true; 					// Starts painting the live preview
-	}
+		paintStatusFlag = true; }					// Starts painting the live preview
+	
 	public void mouseDragged(MouseEvent e) { 		//makes the shape a live-drag
 		bufferImgLive = deepCopy(bufferImg);
 		Graphics buffer2 = bufferImgLive.createGraphics();
@@ -131,6 +109,7 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 		System.out.println(e.getY() + " x " + e.getX());
 		buffer2.setColor(Color.BLACK);
 		fixLiveDirections();
+		
 		switch(button){   						// Switch on which button was pressed
 		case 0: break;  
 		case 1: buffer2.fillRect(startP.x, startP.y, ePoint.x-sPoint.x, ePoint.y-startP.y);  break;			// Draw filled rectangle
@@ -138,22 +117,21 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 		case 3: buffer2.fillOval(startP.x, startP.y, ePoint.x-sPoint.x, ePoint.y-startP.y);  break; 		// Draw filled oval
 		case 4: buffer2.drawOval(startP.x, startP.y, ePoint.x-sPoint.x, ePoint.y-startP.y);  break;			// Draw empty oval
 		case 5: buffer2.drawLine(startP.x, startP.y, ePoint.x, ePoint.y); break; 							// Draw Line
-		default: break;
-		}
+		default: break; }
+		
 		bufferImgLive.flush();
 		repaint();
-		buffer2.dispose();
-	}
+		buffer2.dispose(); }
+	
 	public void mouseReleased(MouseEvent e){		//Final coords for shape
 		paintStatusFlag = false;
-		repaint();
-		System.out.println("mouse released");
 		Graphics buffer = bufferImg.createGraphics();
 		buffer.setColor(Color.BLACK);
 		e.consume();  
 		ePoint.x = e.getX();  
 		ePoint.y = e.getY();
 		fixDirections();
+		
 		switch(button){   //Switch on which button was pressed
 		case 0: break;
 		case 1: buffer.fillRect(startP.x, startP.y, ePoint.x-startP.x, ePoint.y-startP.y);  break;		// Draw filled rectangle
@@ -161,15 +139,12 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 		case 3: buffer.fillOval(startP.x, startP.y, ePoint.x-startP.x, ePoint.y-startP.y);  break; 		// Draw filled oval
 		case 4: buffer.drawOval(startP.x, startP.y, ePoint.x-startP.x, ePoint.y-startP.y);  break;		// Draw empty oval
 		case 5: buffer.drawLine(startP.x, startP.y, ePoint.x, ePoint.y); break; 						// Draw Line
-		default: break;
-		}
-		repaint();
-	}
-	public void mouseMoved(MouseEvent e) {}  
-	public void mouseExited(MouseEvent e){//System.out.println("mouse exited");	//commented out code is for testing
-	}
-	public void mouseEntered(MouseEvent e){//System.out.println("mouse entered");
-	}
-	public void mouseClicked(MouseEvent e){//System.out.println("mouse clicked");
-	}
-}
+		default: break; } }
+	
+	public void mouseMoved(MouseEvent e) {} 
+	
+	public void mouseExited(MouseEvent e){}
+	
+	public void mouseEntered(MouseEvent e){}
+	
+	public void mouseClicked(MouseEvent e){} }
