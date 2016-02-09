c86d755f77e62e502a905f196df0cb03796245a8
Spencer Cornish <spencerjcornish@gmail.com>
2016-02-09 12:07:48 -0700

    Color Choice almost done

--- Paint/src/main/ColorChooser.java
+++ Paint/src/main/ColorChooser.java
@@ -7,20 +7,29 @@ import java.awt.event.*;
 
 public class ColorChooser extends JPanel {
 	private static final long serialVersionUID = -9134135157727043422L;
+	private JFrame frame;
 	protected JColorChooser chooser;
 	private Color colorChoice;
 
 	public ColorChooser() {
 		super(new BorderLayout());
 		chooser = new JColorChooser();
-		JFrame frame = new JFrame("Pick a Color");
+		frame = new JFrame("Pick a Color");
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		JComponent content =  chooser;
 		content.setOpaque(true);
 		frame.setContentPane(content);
-		
 		frame.pack();
-		frame.setVisible(true);
 	}
+	public void showColors()
+	{
+		frame.setVisible(true);
+	
+	}
+	public Color getColor()
+	{
+		return chooser.getColor();
+	}
+	
 
 }
