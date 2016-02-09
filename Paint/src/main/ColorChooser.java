93e8d8888f33930ca911002166e667bd171f82dc
Spencer Cornish <spencerjcornish@gmail.com>
2016-02-09 11:17:08 -0700

    Added ColorChooser Class

---
+++ Paint/src/main/ColorChooser.java
@@ -0,0 +1,26 @@
+package main;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+
+public class ColorChooser extends JPanel {
+	private static final long serialVersionUID = -9134135157727043422L;
+	protected JColorChooser chooser;
+	private Color colorChoice;
+
+	public ColorChooser() {
+		super(new BorderLayout());
+		chooser = new JColorChooser();
+		JFrame frame = new JFrame("Pick a Color");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JComponent content =  chooser;
+		content.setOpaque(true);
+		frame.setContentPane(content);
+		
+		frame.pack();
+		frame.setVisible(true));
+	}
+
+}
