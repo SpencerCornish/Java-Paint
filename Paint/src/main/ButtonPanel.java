7c7f85842b8e05d0e48bb09f7046289583cdb24c
Spencer Cornish <spencerjcornish@gmail.com>
2016-02-08 19:36:10 -0700

    Fixed Indentation

--- Paint/src/main/ButtonPanel.java
+++ Paint/src/main/ButtonPanel.java
@@ -10,75 +10,75 @@ import java.awt.event.*;
 
 
 public class ButtonPanel extends JPanel implements ActionListener, MouseListener { //remove mouselistener :D
-	
+
 	private static final long serialVersionUID = -923758246107102096L;
-	
+
 	Cursor cHair = Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR); 		// Crosshair cursor
 	Cursor cDef = Cursor.getPredefinedCursor(Cursor.DEFAULT_CURSOR); 			// Default cursor
 	private Dimension PREF;					// The preferred button dimension
-    public static ButtonPanel btnP; 		// Instance of Button Panel
-    private JButton buttonClear; 			// Clear the screen
-    private JButton buttonRectangleF; 		// Draw a Filled Rectangle
-    private JButton buttonRectangleE; 		// Draw a Filled Rectangle
-    private JButton buttonOvalF; 			// Draw a Filled Oval
-    private JButton buttonOvalE; 			// Draw an Empty Oval 
-    private JButton buttonLineD; 			// Draw a line
+	public static ButtonPanel btnP; 		// Instance of Button Panel
+	private JButton buttonClear; 			// Clear the screen
+	private JButton buttonRectangleF; 		// Draw a Filled Rectangle
+	private JButton buttonRectangleE; 		// Draw a Filled Rectangle
+	private JButton buttonOvalF; 			// Draw a Filled Oval
+	private JButton buttonOvalE; 			// Draw an Empty Oval 
+	private JButton buttonLineD; 			// Draw a line
 
-    public ButtonPanel()
-    {
-    	
-    	setAlignmentX(Component.CENTER_ALIGNMENT); 				// Button panel alignment? This needs some testing
-        setBackground(Color.GRAY);								// Button panel Background color
-        buttonClear = new JButton("Clear");						//button 0
-        buttonRectangleF = new JButton("Filled Rect.");			//button 1
-        buttonRectangleE = new JButton("Empty Rect."); 			//button 2
-        buttonOvalF = new JButton("Filled Oval");				//button 3
-        buttonOvalE = new JButton("Empty Oval");				//button 4
-        buttonLineD = new JButton("Line");						//button 5
-        add(buttonLineD); 										// Add these. We need some polymorphisim in here! :)
-        add(buttonOvalE);
-        add(buttonOvalF);
-        add(buttonRectangleE);
-        add(buttonRectangleF);
-        add(buttonClear);
-        PREF = buttonRectangleE.getPreferredSize();
-        buttonRectangleF.setPreferredSize(PREF);
-        buttonClear.setPreferredSize(PREF);
-        buttonOvalF.setPreferredSize(PREF);
-        buttonOvalE.setPreferredSize(PREF);
-        buttonLineD.setPreferredSize(PREF);
-        buttonClear.setToolTipText("");
-        buttonClear.addActionListener(this); 
-        buttonRectangleF.addActionListener(this);
-        buttonRectangleE.addActionListener(this);
-        buttonOvalF.addActionListener(this);
-        buttonOvalE.addActionListener(this);
-        buttonLineD.addActionListener(this);  
-    }
-     public static ButtonPanel getInstance()
-    {
-        if(btnP == null)
-           btnP =  new ButtonPanel();
-        return btnP;
-    }
-     public void actionPerformed(ActionEvent ae)
-     {
-         System.out.println(ae.getActionCommand());
-     	 MousePanel.getInstance().setCursor(cHair); //Sets the mouse Cursor to a Cross hair for editing
-     	 
-         if(ae.getSource() == buttonClear) {
-        	 MousePanel.getInstance().setButton(0);
-        	 MousePanel.getInstance().setCursor(cDef);
-        	 MousePanel.getInstance().clearAll();
-        	 //MousePanel.getInstance().repaint();
-         }
-         else if(ae.getSource() == buttonRectangleF) MousePanel.getInstance().setButton(1);
-         else if(ae.getSource() == buttonRectangleE) MousePanel.getInstance().setButton(2);		//feeds button 2 (because it's a rectangle) to button variable in MousePanel class
-         else if(ae.getSource() == buttonOvalF) MousePanel.getInstance().setButton(3);
-         else if(ae.getSource() == buttonOvalE) MousePanel.getInstance().setButton(4);
-         else if(ae.getSource() == buttonLineD) MousePanel.getInstance().setButton(5);
-    	 MousePanel.getInstance().repaint();
-     }
+	public ButtonPanel()
+	{
+
+		setAlignmentX(Component.CENTER_ALIGNMENT); 				// Button panel alignment? This needs some testing
+		setBackground(Color.GRAY);								// Button panel Background color
+		buttonClear = new JButton("Clear");						//button 0
+		buttonRectangleF = new JButton("Filled Rect.");			//button 1
+		buttonRectangleE = new JButton("Empty Rect."); 			//button 2
+		buttonOvalF = new JButton("Filled Oval");				//button 3
+		buttonOvalE = new JButton("Empty Oval");				//button 4
+		buttonLineD = new JButton("Line");						//button 5
+		add(buttonLineD); 										// Add these. We need some polymorphisim in here! :)
+		add(buttonOvalE);
+		add(buttonOvalF);
+		add(buttonRectangleE);
+		add(buttonRectangleF);
+		add(buttonClear);
+		PREF = buttonRectangleE.getPreferredSize();
+		buttonRectangleF.setPreferredSize(PREF);
+		buttonClear.setPreferredSize(PREF);
+		buttonOvalF.setPreferredSize(PREF);
+		buttonOvalE.setPreferredSize(PREF);
+		buttonLineD.setPreferredSize(PREF);
+		buttonClear.setToolTipText("");
+		buttonClear.addActionListener(this); 
+		buttonRectangleF.addActionListener(this);
+		buttonRectangleE.addActionListener(this);
+		buttonOvalF.addActionListener(this);
+		buttonOvalE.addActionListener(this);
+		buttonLineD.addActionListener(this);  
+	}
+	public static ButtonPanel getInstance()
+	{
+		if(btnP == null)
+			btnP =  new ButtonPanel();
+		return btnP;
+	}
+	public void actionPerformed(ActionEvent ae)
+	{
+		System.out.println(ae.getActionCommand());
+		MousePanel.getInstance().setCursor(cHair); //Sets the mouse Cursor to a Cross hair for editing
+
+		if(ae.getSource() == buttonClear) {
+			MousePanel.getInstance().setButton(0);
+			MousePanel.getInstance().setCursor(cDef);
+			MousePanel.getInstance().clearAll();
+			//MousePanel.getInstance().repaint();
+		}
+		else if(ae.getSource() == buttonRectangleF) MousePanel.getInstance().setButton(1);
+		else if(ae.getSource() == buttonRectangleE) MousePanel.getInstance().setButton(2);		//feeds button 2 (because it's a rectangle) to button variable in MousePanel class
+		else if(ae.getSource() == buttonOvalF) MousePanel.getInstance().setButton(3);
+		else if(ae.getSource() == buttonOvalE) MousePanel.getInstance().setButton(4);
+		else if(ae.getSource() == buttonLineD) MousePanel.getInstance().setButton(5);
+		MousePanel.getInstance().repaint();
+	}
 	public void mouseClicked(MouseEvent arg0) {}
 
 	public void mouseEntered(MouseEvent e) {System.out.println("Deez");}
--- Paint/src/main/MousePanel.java
+++ Paint/src/main/MousePanel.java
@@ -11,111 +11,111 @@ import java.awt.image.BufferedImage;
 
 
 public class MousePanel extends JPanel implements MouseListener, MouseMotionListener {
-    
+
 	private static final long serialVersionUID = -8595660419538273421L;
 	public static MousePanel mouseP; 				//Instance of the MousePanel
-    private int button = -1;						//determines which button is pressed based on a number
-    private Point sPoint = new Point(-1, -1);  				//Points used to align shapes and mouse drag
-    private Point ePoint = new Point(-1, -1);
-    private BufferedImage bufferImg;
-    private Graphics g;
-    public MousePanel() {
-        
-    	//setDoubleBuffered(false);       
-        //addMouseListener(new MouseAdapter() {
-        //  public void mousePressed(MouseEvent e) {
-        //  sPoint.x = e.getX();
-        //  sPoint.y = e.getY();
-      //    }
-      //  });
-    	
-    	//FIX ME FIX ME
-    	bufferImg = new BufferedImage(5000,5000, BufferedImage.TYPE_INT_RGB); // FIX ME FIX ME FIX ME FIX ME
-    	//FIX ME FIX ME 
-    	
-    	Graphics buffer = bufferImg.getGraphics();
-    	buffer.setColor(Color.ORANGE);
-    	buffer.fillRect(0, 0, bufferImg.getWidth(),bufferImg.getHeight());
-        addMouseListener(this); 				//Used to do live track, etc.
-        addMouseMotionListener(this); 
-        repaint();
-    }
-    public static MousePanel getInstance()
-    {
-        if(mouseP == null) 						// If there is no instance...
-            mouseP =  new MousePanel(); 		// Make one!
-         return mouseP; 						// Send back the made instance
-        
-    }
+	private int button = -1;						//determines which button is pressed based on a number
+	private Point sPoint = new Point(-1, -1);  				//Points used to align shapes and mouse drag
+	private Point ePoint = new Point(-1, -1);
+	private BufferedImage bufferImg;
+	private Graphics g;
+	public MousePanel() {
 
-    public void paintComponent(Graphics g)
-    {
-    	if (bufferImg == null) {
-    	      bufferImg = (BufferedImage) createImage(getSize().width, getSize().height);
-    	      g = (Graphics2D) bufferImg.getGraphics();
-    	      RenderingHints rh = new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-    	      ((Graphics2D) g).setRenderingHints(rh);
-    	      clearAll();
-    	      
-    	    }
-    	g.drawImage(bufferImg, 0, 0, null);
-    	//getWidth(), getHeight(),
-    }
-    
-    public void setButton(int button) {  			// Sets our button tracking variable
-    	this.button = button;
+		//setDoubleBuffered(false);       
+		//addMouseListener(new MouseAdapter() {
+		//  public void mousePressed(MouseEvent e) {
+		//  sPoint.x = e.getX();
+		//  sPoint.y = e.getY();
+		//    }
+		//  });
 
-    	//buffer.fillRect(30,30, 300, 100);
-    	//repaint();
-    	//ePoint.x = 20; 						//Cleans line tracking variables for next line made
-    	//ePoint.y = 20;
-        //sPoint.x = 200;  
-        //sPoint.y = 200;
-    }
-    public void clearAll()
-    {
-    	Graphics buffer = bufferImg.createGraphics();
-    	buffer.setColor(Color.WHITE);
-    	buffer.fillRect(0, 0, bufferImg.getHeight(), bufferImg.getWidth());
-    	repaint();
-    	
-    }
-    public void mousePressed(MouseEvent e){			// Initial coords for shape
-    	System.out.println("mouse pressed");
-    	e.consume();
-        sPoint.x = e.getX();  
-        sPoint.y = e.getY();
-    	}
-    public void mouseReleased(MouseEvent e){		//Final coords for shape
-    	System.out.println("mouse released");
-    	Graphics buffer = bufferImg.createGraphics();
-    	buffer.setColor(Color.RED);
-    	e.consume();  
-        ePoint.x = e.getX();  
-        ePoint.y = e.getY();
-        switch(button){   						// Switch on which button was pressed.  There may be a better way
-        case 0: break;  // The following shapes have weird offsets,as to make the dragging of a shape feel less insane!
-        case 1: buffer.fillRect(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y);  break;		// Draw filled rectangle
-        case 2: buffer.drawRect(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y);  break; 		// Draw empty rectangle
-        case 3: buffer.fillOval(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y);  break; 		// Draw filled oval
-        case 4: buffer.drawOval(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y);  break;		// Draw empty oval
-        case 5: buffer.drawLine(sPoint.x, sPoint.y, ePoint.x, ePoint.y); break; 							// Draw Line
-        default:break;
-        }
-        repaint();
-    	}
-	public void mouseDragged(MouseEvent e) { 		//makes the shape a live-drag
-    	//e.consume();  
-        //ePoint.x = e.getX();  
-       // ePoint.y = e.getY();
-        System.out.println(e.getY() + " x " + e.getX());
-       // repaint();
+		//FIX ME FIX ME
+		bufferImg = new BufferedImage(5000,5000, BufferedImage.TYPE_INT_RGB); // FIX ME FIX ME FIX ME FIX ME
+		//FIX ME FIX ME 
+
+		Graphics buffer = bufferImg.getGraphics();
+		buffer.setColor(Color.ORANGE);
+		buffer.fillRect(0, 0, bufferImg.getWidth(),bufferImg.getHeight());
+		addMouseListener(this); 				//Used to do live track, etc.
+		addMouseMotionListener(this); 
+		repaint();
+	}
+	public static MousePanel getInstance()
+	{
+		if(mouseP == null) 						// If there is no instance...
+			mouseP =  new MousePanel(); 		// Make one!
+		return mouseP; 						// Send back the made instance
+
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		if (bufferImg == null) {
+			bufferImg = (BufferedImage) createImage(getSize().width, getSize().height);
+			g = (Graphics2D) bufferImg.getGraphics();
+			RenderingHints rh = new RenderingHints(RenderingHints.KEY_TEXT_ANTIALIASING,RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+			((Graphics2D) g).setRenderingHints(rh);
+			clearAll();
+
 		}
+		g.drawImage(bufferImg, 0, 0, null);
+		//getWidth(), getHeight(),
+	}
+
+	public void setButton(int button) {  			// Sets our button tracking variable
+		this.button = button;
+
+		//buffer.fillRect(30,30, 300, 100);
+		//repaint();
+		//ePoint.x = 20; 						//Cleans line tracking variables for next line made
+		//ePoint.y = 20;
+		//sPoint.x = 200;  
+		//sPoint.y = 200;
+	}
+	public void clearAll()
+	{
+		Graphics buffer = bufferImg.createGraphics();
+		buffer.setColor(Color.WHITE);
+		buffer.fillRect(0, 0, bufferImg.getHeight(), bufferImg.getWidth());
+		repaint();
+
+	}
+	public void mousePressed(MouseEvent e){			// Initial coords for shape
+		System.out.println("mouse pressed");
+		e.consume();
+		sPoint.x = e.getX();  
+		sPoint.y = e.getY();
+	}
+	public void mouseReleased(MouseEvent e){		//Final coords for shape
+		System.out.println("mouse released");
+		Graphics buffer = bufferImg.createGraphics();
+		buffer.setColor(Color.RED);
+		e.consume();  
+		ePoint.x = e.getX();  
+		ePoint.y = e.getY();
+		switch(button){   						// Switch on which button was pressed.  There may be a better way
+		case 0: break;  // The following shapes have weird offsets,as to make the dragging of a shape feel less insane!
+		case 1: buffer.fillRect(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y);  break;		// Draw filled rectangle
+		case 2: buffer.drawRect(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y);  break; 		// Draw empty rectangle
+		case 3: buffer.fillOval(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y);  break; 		// Draw filled oval
+		case 4: buffer.drawOval(sPoint.x, sPoint.y, ePoint.x-sPoint.x, ePoint.y-sPoint.y);  break;		// Draw empty oval
+		case 5: buffer.drawLine(sPoint.x, sPoint.y, ePoint.x, ePoint.y); break; 							// Draw Line
+		default:break;
+		}
+		repaint();
+	}
+	public void mouseDragged(MouseEvent e) { 		//makes the shape a live-drag
+		e.consume();  
+		ePoint.x = e.getX();  
+		ePoint.y = e.getY();
+		System.out.println(e.getY() + " x " + e.getX());
+		repaint();
+	}
 	public void mouseMoved(MouseEvent e) { } //This will be useful soon, adding mouse coords to a tooltip in the bottom right corner    
 	public void mouseExited(MouseEvent e){//System.out.println("mouse exited");
-		}
-    public void mouseEntered(MouseEvent e){//System.out.println("mouse entered");
-    	}
-    public void mouseClicked(MouseEvent e){//System.out.println("mouse clicked");
-    }
+	}
+	public void mouseEntered(MouseEvent e){//System.out.println("mouse entered");
+	}
+	public void mouseClicked(MouseEvent e){//System.out.println("mouse clicked");
+	}
 }
