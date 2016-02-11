6d3f5a9c2b0d65044410d55a1a173acc43cf2498
Nate Tranel <njtranel@gmail.com>
2016-02-11 12:23:01 -0700

    small fix, looks better

--- Paint/src/main/ButtonIcon.java
+++ Paint/src/main/ButtonIcon.java
@@ -30,7 +30,7 @@ public class ButtonIcon implements Icon{
 		case 4: g.setColor(Color.BLACK); g.drawOval(x, y, getIconWidth(), getIconHeight()); break;
 		case 5: g.setColor(Color.BLACK); g.drawLine(8, 18, 40, 8); break;
 		case 6: g.setColor(ColorPanel.getInstance().getColor(2)); g.fillRect(5, y, 10, 15); break;
-		case 7: g.setColor(ColorPanel.getInstance().getColor(1)); g.drawRect(5, y, 10, 15); break;
+		case 7: g.setColor(ColorPanel.getInstance().getColor(1)); g.fillRect(5, y, 10, 15); break;
 		case 8: g.setColor(ColorPanel.getInstance().getColor(0)); g.fillRect(5, y, 10, 15); break;
 		}
 	}
