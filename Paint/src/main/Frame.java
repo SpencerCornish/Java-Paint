ac0a63f56d02296db2973cde1dd212e911375713
Spencer Cornish <spencerjcornish@gmail.com>
2016-02-12 11:39:37 -0700

    Added JMenuBar, but it's not working just yet!

--- Paint/src/main/Frame.java
+++ Paint/src/main/Frame.java
@@ -16,6 +16,7 @@ public class Frame extends JFrame
 
 	private static final long serialVersionUID = 986906333940524590L; //This is apparently important.  It got rid of a warning, so...
 	public static Frame frame;
+	private JMenuBar menuBar = new JMenuBar();
 	private final Dimension MINSIZE = new Dimension(800,600);
 	private final TitledBorder CTITLE = new TitledBorder("Colors");
 	private final TitledBorder BTITLE = new TitledBorder("Shapes");
@@ -27,6 +28,7 @@ public class Frame extends JFrame
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  						// Exit on close
 		Container c = getContentPane(); 										// Returns frame
 		JPanel westContainer = new JPanel();
+		makeMenu();
 				
 		setMinimumSize(MINSIZE);  								// Minimum window size allowed, subject to change
 		setSize(MINSIZE);
@@ -42,6 +44,20 @@ public class Frame extends JFrame
 		c.add(westContainer, BorderLayout.WEST); 					// Puts ButtonPanel West
 		c.add(MousePanel.getInstance(), BorderLayout.CENTER); 					// Centers canvas
 		setVisible(true); 
+	}
+	public void makeMenu()
+	{
+		
+		JMenu fileMenu = new JMenu("File");
+		JMenu editMenu = new JMenu("Edit");
+		JMenu helpMenu = new JMenu("Help");
+		
+		menuBar.add(fileMenu);
+		menuBar.add(editMenu);
+		menuBar.add(helpMenu);
+		
+		this.setJMenuBar(menuBar);
+		
 	}
 	public static Frame getInstance()	//gets instance of frame and makes one if none exist
 	{
