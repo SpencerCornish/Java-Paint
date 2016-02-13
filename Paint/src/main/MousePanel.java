83fe48a09d8d7ba914656474097e98e7d25a7b52
Nate Tranel <njtranel@gmail.com>
2016-02-13 13:37:25 -0700

    Saves and opens!!!

--- Paint/src/main/MousePanel.java
+++ Paint/src/main/MousePanel.java
@@ -24,6 +24,7 @@ import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
 import java.awt.image.WritableRaster;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 
 
@@ -39,10 +40,10 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 	private Point ePoint = new Point();				//end Point
 
 	public MousePanel() {
-		bufferImg = new BufferedImage(2000,2000, BufferedImage.TYPE_INT_RGB); 
+		bufferImg = new BufferedImage(2000, 2000, BufferedImage.TYPE_INT_RGB); 
 		Graphics buffer = bufferImg.getGraphics();
 		buffer.setColor(Color.WHITE);
-		buffer.fillRect(0, 0, bufferImg.getWidth(),bufferImg.getHeight());
+		buffer.fillRect(0, 0, 2000, 2000);
 
 		addMouseListener(this); 					//Used to get coordinates of shape
 		addMouseMotionListener(this); 				//Used for mouseDragged()
@@ -56,7 +57,7 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 	public void paintComponent(Graphics g) {
 		if (bufferImg == null) 
 		{					//fills the buffer if it hasn't been filled already
-			bufferImg = (BufferedImage) createImage(getSize().width, getSize().height);
+			bufferImg = (BufferedImage) createImage(2000, 2000);
 			g = (Graphics2D) bufferImg.getGraphics();
 			clearAll(); 
 		}
@@ -77,7 +78,7 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 	public void clearAll() { 							//clears mousepanel by painting over the background image
 		Graphics buffer = bufferImg.createGraphics();
 		buffer.setColor(Color.WHITE);
-		buffer.fillRect(0, 0, bufferImg.getHeight(), bufferImg.getWidth());
+		buffer.fillRect(0, 0, 2000, 2000);
 		repaint();
 	}
 	public void fixDirections()	{						//makes sure final coordinates are good to paint with
@@ -176,9 +177,9 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 	    chooser.setFileFilter(filter);
 	    int returnVal = chooser.showSaveDialog(getParent());
 	    if(returnVal == JFileChooser.APPROVE_OPTION) {
-	    	ImageIO.write(bufferImg, "PNG", new File(chooser.getSelectedFile().getName()));	//new File("Picture.png")
-	    	System.out.println("You chose to save this file: " +
-	            chooser.getSelectedFile().getName());
+	    	FileOutputStream os = new FileOutputStream(chooser.getSelectedFile().getPath() + ".png");
+	    	ImageIO.write(bufferImg, "png", os);
+	    	os.close();
 	    }
 	}
 	
@@ -190,8 +191,6 @@ public class MousePanel extends JPanel implements MouseListener, MouseMotionList
 	    int returnVal = chooser.showOpenDialog(getParent());
 	    if(returnVal == JFileChooser.APPROVE_OPTION) {
 	    	bufferImg = ImageIO.read(chooser.getSelectedFile());
-	    	System.out.println("You chose to open this file: " +
-	            chooser.getSelectedFile().getName());
 	    }
 		repaint();
 	}
